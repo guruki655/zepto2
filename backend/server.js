dd833a5dffa6f5495ca6b5f8c22ce3389e1be49a
guruki655 <gurukiran655@gmail.com>
2025-05-17 13:51:55 +0530

    server listte

--- backend/server.js
+++ backend/server.js
@@ -40,4 +40,4 @@ mongoose.connect(MONGODB_URI, {
 
 // Server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, '0.0.0.0', () => console.log(`Server running on port ${PORT}`));
